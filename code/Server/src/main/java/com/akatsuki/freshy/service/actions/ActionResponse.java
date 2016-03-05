d06e49c90fef78539ccbdb685a3b3e2dde848455
Marin Luketin <marin.luketin@gmail.com>
2016-03-05 14:59:27 +0100

    Add ActionResponse class and example of returning response

---
+++ code/Server/src/main/java/com/akatsuki/freshy/service/actions/ActionResponse.java
@@ -0,0 +1,14 @@
+package com.akatsuki.freshy.service.actions;
+
+public class ActionResponse {
+
+  private final String response;
+
+  public ActionResponse(String response) {
+    this.response = response;
+  }
+
+  public String getResponse() {
+    return response;
+  }
+}
--- code/Server/src/main/java/com/akatsuki/freshy/service/controller/CreateAction.java
+++ code/Server/src/main/java/com/akatsuki/freshy/service/controller/CreateAction.java
@@ -7,6 +7,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.json.JSONObject;
+
+import com.akatsuki.freshy.service.actions.ActionResponse;
+import com.google.gson.Gson;
+
 @Path("/create")
 public class CreateAction {
 
@@ -14,13 +19,15 @@ public class CreateAction {
   @Path("/create")
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.TEXT_PLAIN)
-  public String create() {
+  public String create(String s) {
 
     System.out.println("CREATE METODA");
 
+    System.out.println("STRING" + s);
 
+    ActionResponse res = new ActionResponse("NOT OK ACTION RESP");
 
-    return "NOT OK SRYS";
+    return (new Gson()).toJson(res);
   }
 
   @GET
@@ -29,6 +36,8 @@ public class CreateAction {
   public String getIt() {
 
       System.out.println("GET IT");
+
+
 
       return "Got it!";
   }
